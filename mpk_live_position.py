4e43d5a1e8dc6c5b46cc9fd8411059270ce8be35
AlvdelPor <soko-ma@nurt.net.pl>
2026-07-17 22:56:24 +0200

    mpk wroclaw live vehicles position API

--- mpk_live_position.py
+++ mpk_live_position.py
@@ -1,31 +1,35 @@
+# mpk wroclaw live vehicles position API
 import requests
 
 base_url = "https://api.open-data.cui.wroclaw.pl/od3-records/data/14"
 
-def get_bus_data(side_number):
-    needed_bus_data = None
+def get_vehicle_data(line_number):
+    needed_vehicle_data = []
+    i = int(0)
     response = requests.get(base_url)
     mpk_data = response.json()
     while mpk_data["next"] is not None:
         if response.status_code == 200:
-            mpk_data = response.json()
-            for bus in range(len(mpk_data["results"])):
-                if mpk_data["results"][bus]["data"]["Nr_Boczny"] == side_number:
-                    needed_bus_data = mpk_data["results"][bus]
+            for vehicle in range(len(mpk_data["results"])):
+                if mpk_data["results"][vehicle]["data"]["Nazwa_Linii"] == line_number:
+                    needed_vehicle_data.append(mpk_data["results"][vehicle])
         else:
             print(f"Failed to retrieve data {response.status_code}")
         response = requests.get(mpk_data["next"])
         mpk_data = response.json()
-    return needed_bus_data
+    return needed_vehicle_data
 
-def print_bus_data(bus_data):
-    if bus_data:
-        print(f"id: {bus_data["id"]}")
-        print(f"numer boczny: {bus_data["data"]["Nr_Boczny"]}")
-        print(f"linia: {bus_data["data"]["Nazwa_Linii"]}")
-        print(f"długosc: {bus_data["data"]["Ostatnia_Pozycja_Dlugosc"]}")
-        print(f"szerokosc: {bus_data["data"]["Ostatnia_Pozycja_Szerokosc"]}")
-        print(f"aktualizacja: {str(bus_data["harvested_at"])[0:10] + " " + str(bus_data["harvested_at"])[11:19]}")
+def print_vehicle_data(vehicle_data):
+    if vehicle_data:
+        i = int(0)
+        for i in range(len(vehicle_data)):
+            print(f"id: {vehicle_data[i]["id"]}")
+            print(f"numer boczny: {vehicle_data[i]["data"]["Nr_Boczny"]}")
+            print(f"linia: {vehicle_data[i]["data"]["Nazwa_Linii"]}")
+            print(f"długosc: {vehicle_data[i]["data"]["Ostatnia_Pozycja_Dlugosc"]}")
+            print(f"szerokosc: {vehicle_data[i]["data"]["Ostatnia_Pozycja_Szerokosc"]}")
+            print(f"aktualizacja: {str(vehicle_data[i]["harvested_at"])[0:10] + " " + str(vehicle_data[i]["harvested_at"])[11:19]}")
+            print("")
 
-number = int(3015)
-print_bus_data(get_bus_data(number))
+line = str(715)
+print_vehicle_data(get_vehicle_data(line))
