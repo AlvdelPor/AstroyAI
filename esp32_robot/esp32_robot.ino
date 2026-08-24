bcd371d9ad92c6da8caa9ac6673026c72ddd7a03
AlvdelPor <soko-ma@nurt.net.pl>
2026-08-24 21:20:09 +0200

    AstroyAI

--- esp32_robot/esp32_robot.ino
+++ esp32_robot/esp32_robot.ino
@@ -6,15 +6,17 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+// --- NOWE PINY I2C ---
+#define I2C_SDA 21
+#define I2C_SCL 19 // Przepnij SCK do tego pinu!
+
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-#define LED_PIN 2
 bool deviceConnected = false;
 String currentEmotion = "IDLE";
 
-// Unikalne adresy dla naszego Bluetooth
 #define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
@@ -25,7 +27,6 @@ class MyServerCallbacks: public BLEServerCallbacks {
 
 class MyCallbacks: public BLECharacteristicCallbacks {
     void onWrite(BLECharacteristic *pChar) {
-        // POPRAWKA: W nowej bibliotece getValue() zwraca od razu typ String
         String value = pChar->getValue();
         if (value.length() > 0) {
             currentEmotion = value;
@@ -34,14 +35,12 @@ class MyCallbacks: public BLECharacteristicCallbacks {
 };
 
 void setup() {
-    pinMode(LED_PIN, OUTPUT);
-    
-    // Inicjalizacja ekranu
+    // Ręczne wymuszenie nowych pinów dla komunikacji I2C
+    Wire.begin(I2C_SDA, I2C_SCL);
     display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
     display.clearDisplay();
     
-    // Inicjalizacja Bluetooth
-    BLEDevice::init("Robot_Oliwii");
+    BLEDevice::init("Astroy");
     BLEServer *pServer = BLEDevice::createServer();
     pServer->setCallbacks(new MyServerCallbacks());
     BLEService *pService = pServer->createService(SERVICE_UUID);
@@ -58,36 +57,45 @@ void setup() {
 
 void drawFace(String emotion) {
     display.clearDisplay();
-    display.setTextSize(2);
-    display.setTextColor(WHITE);
-    
-    // Proste minki tekstowe do testów
-    if(emotion == "HAPPY") { display.setCursor(35, 25); display.print("^___^"); }
-    else if(emotion == "SAD") { display.setCursor(35, 25); display.print("T___T"); }
-    else if(emotion == "ANGRY") { display.setCursor(35, 25); display.print(">___<"); }
-    else if(emotion == "THINKING") { display.setCursor(35, 25); display.print("O___-"); }
-    else if(emotion == "CONFUSED") { display.setCursor(35, 25); display.print("@___@"); }
-    else if(emotion == "SURPRISED") { display.setCursor(35, 25); display.print("O___O"); }
-    else if(emotion == "SCARED") { display.setCursor(35, 25); display.print("!___!"); }
+    long time = millis();
+    bool isBlinking = (time % 3000 < 200); // Szybkie mrugnięcie co 3 sekundy
+
+    int eyeW = 28;  // Szerokość oka
+    int eyeH = 34;  // Wysokość oka
+    int leftX = 20; // Pozycja lewego oka
+    int rightX = 128 - 20 - eyeW; // Pozycja prawego oka
+    int y = 15;     // Pozycja w pionie
+
+    if (isBlinking) { 
+        eyeH = 4; // Zamknięte oczy (kreski)
+        y = 30; 
+    }
+
+    if (emotion == "HAPPY") {
+        // Szczęśliwe, wygięte w łuk oczy
+        display.fillRoundRect(leftX, y, eyeW, eyeH, 8, WHITE);
+        display.fillRoundRect(rightX, y, eyeW, eyeH, 8, WHITE);
+        display.fillRect(0, y + 15, 128, 20, BLACK); // Ucinamy dół prostokątem
+    }
+    else if (emotion == "ANGRY") {
+        // Groźne, ścięte do środka brwi
+        display.fillRoundRect(leftX, y, eyeW, eyeH, 8, WHITE);
+        display.fillRoundRect(rightX, y, eyeW, eyeH, 8, WHITE);
+        display.fillTriangle(leftX, y-5, leftX+eyeW+10, y+15, leftX, y+15, BLACK);
+        display.fillTriangle(rightX+eyeW, y-5, rightX-10, y+15, rightX+eyeW, y+15, BLACK);
+    }
     else { 
-        // Stan IDLE z animacją lekkiego ruchu
-        int yOffset = (millis() / 600) % 2 == 0 ? 2 : -2;
-        display.setCursor(35, 25 + yOffset);
-        display.print("o___o");
+        // IDLE / THINKING / SAD - rozglądanie się
+        int offsetX = (time / 1500) % 2 == 0 ? 6 : -6; 
+        if (emotion == "THINKING") offsetX = 0; // Skupienie na wprost
+
+        display.fillRoundRect(leftX + offsetX, y, eyeW, eyeH, 8, WHITE);
+        display.fillRoundRect(rightX + offsetX, y, eyeW, eyeH, 8, WHITE);
     }
     display.display();
 }
 
-void pulseLED() {
-    // Matematyczny efekt gładkiego "oddychania" diody
-    float val = (exp(sin(millis()/2000.0*PI)) - 0.36787944)*108.0;
-    analogWrite(LED_PIN, val);
-}
-
 void loop() {
-    if(currentEmotion == "IDLE") pulseLED();
-    else analogWrite(LED_PIN, 0); // Wyłącz led podczas pokazywania emocji
-    
     drawFace(currentEmotion);
-    delay(50);
+    delay(50); // Krótkie opóźnienie dla płynności klatek
 }
